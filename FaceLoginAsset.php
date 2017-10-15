a5768adc40f3ab39a52a841cf51c0fb6df886957
macbook <luisarmando1234@gmail.com>
2017-10-15 17:59:25 -0500

    added face code

---
+++ FaceLoginAsset.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: macbook
+ * Date: 10/15/17
+ * Time: 5:53 PM
+ */
---
+++ FaceLoginWidget.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: macbook
+ * Date: 10/15/17
+ * Time: 5:51 PM
+ */
+
+namespace inquid\facelogin;
+
+
+class FaceLoginWidget
+{
+
+}
