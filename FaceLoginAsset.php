acd55f8a8ead6fe2f14a05f0394b7fcf80bef378
macbook <luisarmando1234@gmail.com>
2017-10-15 17:59:36 -0500

    added face code

--- FaceLoginAsset.php
+++ FaceLoginAsset.php
@@ -5,3 +5,11 @@
  * Date: 10/15/17
  * Time: 5:53 PM
  */
+namespace inquid\facelogin;
+use yii\web\AssetBundle;
+class FaceLoginAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/inquid/yii2-face-login/assets/';
+    public $js = ['capture.js'];
+    public $depends = ['main.css'];
+}
